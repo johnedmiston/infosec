290bc0ede95cc421ac6d861aa58493394420e7ea
Alexander-Weinhart <alex@onlinedesignsbyalex.com>
2025-07-29 17:19:33 -0400

    server testing 6

--- js/jquery.js
+++ js/jquery.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const urlParams = new URLSearchParams(window.location.search);
     const articleId = parseInt(urlParams.get('article')) || 0;
     const articleData = article[articleId];
-     $("#navbar-plh").load("html/elements/navbar.html", function() {
+     $("#navbar-plh").load("navbar.html", function() {
          console.log("Navbar loaded successfully");
          document.dispatchEvent(new Event("navbarLoaded"));
      });
-    $("#category-plh").load("/html/category.html", function() {
+    $("#category-plh").load("category.html", function() {
         console.log("Category loaded successfully");
         document.dispatchEvent(new Event("categoryLoaded"));
     });
-    $("#footer-plh").load("html/elements/footer.html", function() {
+    $("#footer-plh").load("footer.html", function() {
         console.log("Footer loaded successfully");
         document.dispatchEvent(new Event("footerLoaded"));
     });
